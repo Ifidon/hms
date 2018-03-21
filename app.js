3a84b2b82ecf82ad11c1f9dd7c7c6ebfaeddc75e
Ifidon <e_fidon@yahoo.com>
2018-03-21 13:33:24 +0100

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var bootstrap = require('bootstrap');
+var bootstrap = require('express-bootstrap-service');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -39,6 +39,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bootstrap.serve)
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var patientRouter = require('./patients.js');
+var Patients = require('../models/patients');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', (req, res) => {
+  Patients.find({patient_id: req.body.search})
+  .then((patients) => {
+    console.log(patients)
+    // res.send(patient[patient_id]
+    res.render('search_result', {data: patients})
+  })
 });
 
 module.exports = router;
