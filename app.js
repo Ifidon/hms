50fe17cd75c775d7c6f1472703fbff2abea9db66
Don <e_fidon@yahoo.com>
2018-10-05 14:17:48 +0100

    logging

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error', {title: 'Error - HealthMax'});
+  log.error(err)
 });
 
 module.exports = app;
